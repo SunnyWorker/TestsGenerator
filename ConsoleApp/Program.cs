9af22fe0022a55a050bdbe543d914c29ec71e530
artem <artem.khotin@gmail.com>
2022-11-01 02:19:16 +0300

    Checkers added

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,23 +7,49 @@ public class Program
     public async static Task Main()
     {
         string fileListPath;
-        string testFilesPath;
+        string testFilesPath = null;
         int maxReadingTasks;
         int maxProcessingTasks;
-        int maxWritingTasks;
-        Console.WriteLine("Введите путь к файлу со списком исходных файлов для генерации тестов:");
+        int maxWritingTasks;        
         List<string> files = new();
-        fileListPath = Console.ReadLine();
-        TextReader textReader = File.OpenText(fileListPath);
-        string s;
-        while ((s = textReader.ReadLine())!=null)
+        bool success = false;
+        while (!success)
         {
-            files.Add(s);
+            try
+            {
+                Console.WriteLine("Введите путь к файлу со списком исходных файлов для генерации тестов:");
+                fileListPath = Console.ReadLine();
+                TextReader textReader = File.OpenText(fileListPath);
+                string s;
+                while ((s = textReader.ReadLine())!=null)
+                {
+                    files.Add(s);
+                }
+
+                success = true;
+            }
+            catch (IOException e)
+            {
+                success = false;
+                Console.WriteLine("Введённые данные некорректны. Повторите попытку!");
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                success = false;
+                Console.WriteLine("Введённые данные некорректны. Повторите попытку!");
+            }
+            
         }
-        
-        Console.WriteLine("Введите путь к директории, где будут храниться тестовые файлы:");
-        testFilesPath = Console.ReadLine();
-        
+
+        success = false;
+        while (!success)
+        {
+            Console.WriteLine("Введите путь к директории, где будут храниться тестовые файлы:");
+            testFilesPath = Console.ReadLine();
+            success = Directory.Exists(testFilesPath);
+            if(!success) Console.WriteLine("Директории не существует. Повторите попытку!"); 
+        }
+
         Console.WriteLine("Введите максимальное количество файлов, загружаемых за раз:"); 
         maxReadingTasks = Convert.ToInt32(Console.ReadLine());
         
